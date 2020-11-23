24ba4f9e116f47e72261286dd805360390ef2971
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-23 10:13:32 +0530

    AddressBookJDBC : Retrive Data from Database

---
+++ src/test/java/com/blz/addressBook/AddressBookServiceTest.java
@@ -0,0 +1,25 @@
+package com.blz.addressBook;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.blz.addressBook.AddressBookService.IOService;
+
+public class AddressBookServiceTest {
+
+	static AddressBookService addressBookService;
+	
+	@BeforeClass
+	public static void AddressBookServiceObj() {
+		addressBookService = new AddressBookService();
+	}
+
+	@Test
+	public void givenAddressBookContactsInDB_WhenRetrieved_ShouldMatchContactsCount() throws AddressBookException {
+		List<AddressBookData> addressBookData = addressBookService.readAddressBookData(IOService.DB_IO);
+		Assert.assertEquals(3, addressBookData.size());
+	}
+}
